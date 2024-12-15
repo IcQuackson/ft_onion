fec39b3b9ea8bf0fc7563e4e19d47370f3f141ae
IcQuackson <pedrorenato1998@gmail.com>
2024-12-15 22:09:11 +0000

    feature: further fortified ssh
    - Disabled password auth
    - Enable pub key auth, ssh protocol 2 and secure ciphers for communication

--- src/ssh/docker-entrypoint.sh
+++ src/ssh/docker-entrypoint.sh
@@ -5,20 +5,49 @@ set -e
 # Expected format: "user1:pass1,user2:pass2"
 echo "Creating users..."
 if [ -n "$USER_LIST" ]; then
-  IFS=',' read -ra USER_ARRAY <<< "$USER_LIST"
-  for userpass in "${USER_ARRAY[@]}"; do
-	echo "Processing userpass: $userpass"
-    IFS=':' read -r username password <<< "$userpass"
-    # Skip if username or password is missing
-    [ -z "$username" ] && continue
-    [ -z "$password" ] && continue
+	IFS=',' read -ra USER_ARRAY <<< "$USER_LIST"
+	for userpass in "${USER_ARRAY[@]}"; do
+		echo "Processing userpass: $userpass"
+		IFS=':' read -r username password <<< "$userpass"
+		# Skip if username or password is missing
+		[ -z "$username" ] && continue
+		[ -z "$password" ] && continue
 
-    # Create user if not exists
-    if ! id "$username" &>/dev/null; then
-      useradd -m -s /bin/bash "$username"
-      echo "$username:$password" | chpasswd
-    fi
-  done
+		# Create user if not exists
+		if ! id "$username" &>/dev/null; then
+			useradd -m -s /bin/bash "$username"
+			echo "$username:$password" | chpasswd
+		fi
+	done
+fi
+
+# Add SSH keys for users from SSH_KEY_LIST
+echo "Adding SSH keys..."
+# echo keys exist or not
+echo "SSH_KEY_LIST: $SSH_KEY_LIST"
+if [ -n "$SSH_KEY_LIST" ]; then
+	echo "SSH_KEY_LIST is not empty"
+	IFS=',' read -ra KEY_ARRAY <<< "$SSH_KEY_LIST"
+	for key_entry in "${KEY_ARRAY[@]}"; do
+		echo "Processing key_entry: $key_entry"
+		IFS=':' read -r username public_key <<< "$key_entry"
+		# Skip if username or public_key is missing
+		[ -z "$username" ] && continue
+		[ -z "$public_key" ] && continue
+
+		# Add public key to the user's authorized_keys
+		if id "$username" &>/dev/null; then
+			echo "Adding key to user: $username"
+			echo "Key: $public_key"
+			mkdir -p /home/"$username"/.ssh
+			echo "$public_key" > /home/"$username"/.ssh/authorized_keys
+			chown -R "$username":"$username" /home/"$username"/.ssh
+			chmod 700 /home/"$username"/.ssh
+			chmod 600 /home/"$username"/.ssh/authorized_keys
+		else
+			echo "User $username does not exist; skipping key addition."
+		fi
+	done
 fi
 
 echo "Starting SSH server..."
